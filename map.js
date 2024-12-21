cf903b6269b6609b29693c18ab28ed79f80e27b9
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2024-12-21 23:07:10 +0530

    function domainNamesOf added

--- map.js
+++ map.js
@@ -66,7 +66,11 @@ const charCodesOf = function (strings) {
 };
 
 // extract domain names from ["user1@gmail.com", "admin@yahoo.com"] => ["gmail.com", "yahoo.com"]
-const domainNamesOf = function (emails) { };
+const domainNamesOf = function (emails) { 
+  return emails.map(function (email) {
+    return email.includes("gmail") ? "gmail.com" : "yahoo.com";
+  });
+};
 
 // split words in ["hello world", "goodbye moon"] => [["hello", "world"], ["goodbye", "moon"]]
 const splitWordsOf = function (strings) { };
