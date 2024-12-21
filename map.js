a806008ab20b000b152d96c869c8bf49016da5c1
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2024-12-21 23:01:31 +0530

    function charCodesOf addded

--- map.js
+++ map.js
@@ -59,7 +59,11 @@ const negatedBooleansOf = function (booleans) {
 
 // character codes of ["a", "b", "c"] => [97, 98, 99]
 // Use the `charCodeAt` method on each string
-const charCodesOf = function (strings) { };
+const charCodesOf = function (strings) { 
+  return strings.map(function (char) {
+    return char.charCodeAt(0);
+  });
+};
 
 // extract domain names from ["user1@gmail.com", "admin@yahoo.com"] => ["gmail.com", "yahoo.com"]
 const domainNamesOf = function (emails) { };
