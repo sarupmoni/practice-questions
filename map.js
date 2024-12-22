8da307b4d9d919df905c1ba30081bf87b4aef2c4
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2024-12-22 09:56:59 +0530

    flattenedArraysOf added

--- map.js
+++ map.js
@@ -178,7 +178,11 @@ const wordLengthsOf = function (strings) {
 };
 
 // flatten nested arrays of [[1, [2, 3]], [4, [5, 6]]] => [[1, 2, 3], [4, 5, 6]]
-const flattenedArraysOf = function (arrays) { };
+const flattenedArraysOf = function (arrays) {
+  return arrays.map(function (array) {
+    return array.flat();
+  });
+};
 
 // sort letters in ["cat", "bat", "rat"] alphabetically => ["act", "abt", "art"]
 const sortedLettersOf = function (strings) { };
