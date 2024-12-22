db575919477e9751085e72ec900b87cf49fdd73c
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2024-12-22 16:35:53 +0530

    markLargestNumber added

--- map.js
+++ map.js
@@ -417,12 +417,18 @@ const differencesFromMean = function (numbers) {
 };
 
 // map each string to its frequency in ["apple", "banana", "apple", "apple", "banana"] => [3, 2, 3, 3, 2]
-const stringFrequencies = function (strings) {
-  
-};
+const stringFrequencies = function (strings) { };
 
 // mark the largest number in an array as true, others as false in [1, 3, 2] => [false, true, false]
-const markLargestNumber = function (numbers) { };
+const markLargestNumber = function (numbers) {
+  const maximumNum = numbers.reduce(function (max, number) {
+    return Math.max(max, number);
+  });
+
+  return numbers.map(function (number) {
+    return number === maximumNum;
+  });
+};
 
 // normalize scores based on a curve: first find the max score, then subtract the mean, and scale the results to a range of 0-100 in [{ name: "Alice", score: 80 }, { name: "Bob", score: 100 }, { name: "Charlie", score: 90 }] => [60, 100, 80]
 // Steps: Find max score, calculate mean, normalize each score.
