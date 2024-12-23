46e2909c6f1d94212bc9dbfa65d676270cb142d7
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2024-12-23 08:07:50 +0530

    filterNumbersGreaterThanTen added

--- filter.js
+++ filter.js
@@ -27,7 +27,11 @@ const filterActiveUsers = function (users) {
 };
 
 // numbers greater than 10 [5, 12, 7, 18, 3] => [12, 18]
-const filterNumbersGreaterThanTen = function (numbers) { };
+const filterNumbersGreaterThanTen = function (numbers) {
+  return numbers.filter(function (number) {
+    return number >= 10;
+  });
+};
 
 // books with more than 200 pages [{title: "Book 1", pages: 150}, {title: "Book 2", pages: 250}] => [{title: "Book 2", pages: 250}]
 const filterLongBooks = function (books) { };
